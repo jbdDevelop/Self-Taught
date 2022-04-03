2b70fa05bad0f64c83f7cfa96105ef57f521ff1d
jbdDevelop <54418760+jbdDevelop@users.noreply.github.com>
2022-04-02 21:23:58 -0500

    Learning
    Self Taught using Intro to Java 8th edition Y. Daniel Liang

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Display Message Welcome to Java! to the console
+		System.out.println("Welcome to Java!");
+	}
+
+}
---
+++ WelcomeInMessageDialogBox.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class WelcomeInMessageDialogBox {
+
+	public static void main(String[] args) {
+		// display Welcome to Java! in a message dialog box
+		JOptionPane.showMessageDialog(null, "Welcome to Java!");
+
+	}
+
+}
